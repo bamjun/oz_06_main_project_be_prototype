03e5f3b1a549204ee8c44b5bf2643383f11db08d
bamjun <khy5116@naver.com>
2024-12-09 18:41:26 +0900

    first

---
+++ django/a_apis/api/legal.py
@@ -0,0 +1,9 @@
+from ninja import Router
+
+from django.shortcuts import render
+router = Router()
+
+
+@router.get("/privacy")
+def eu_consent(request):
+    return render(request, "legal/eu_consent.html")
